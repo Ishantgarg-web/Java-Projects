8d526340df203cf7987d113b890d089bd6b19745
Ishantgarg-web <ishantkumar1412@gmail.com>
2025-09-06 11:31:54 +0530

    Bug Fix: correct operator constants

--- BrainCalculator/src/main/java/org/example/constants/OperatorConstants.java
+++ BrainCalculator/src/main/java/org/example/constants/OperatorConstants.java
@@ -3,8 +3,8 @@ package org.example.constants;
 public class OperatorConstants {
 
     public static String PLUS = "+";
-    public static String SUBTRACT = "+";
-    public static String MULTIPLY = "+";
-    public static String DIVIDE = "+";
+    public static String SUBTRACT = "-";
+    public static String MULTIPLY = "*";
+    public static String DIVIDE = "/";
 
 }
